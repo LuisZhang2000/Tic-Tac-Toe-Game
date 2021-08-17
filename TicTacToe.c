789226377434b7829a50f79580de889a53d9aeff
Luis Zhang <42627737+LuisZhang2000@users.noreply.github.com>
2021-08-17 18:32:58 +1000

    Fixed bug, added more comments
    Fixed bug where player could replace an existing sign with their own, added additional comments to clarify code

--- TicTacToe.c
+++ TicTacToe.c
@@ -8,26 +8,33 @@ void win(int player, int winner, int pos[]) {
     printf("-+-+-\n");
     printf("%c|%c|%c\n", pos[6], pos[7], pos[8]);
 
+    // if winner == 1 i.e. there is a winner, print the winner
+    // otherwise it's a draw
     if (winner) {
         printf("Player %d is the winner\n", player);
     } else {
-        printf("Math draw\n");
+        printf("Match draw\n");
     }
 }
 
 int main() {
-    int winner = 0, count = 0;
-    int pos[9];     // this is our board
-    int index, sign, player, flag, i, k, j;
+    int winner = 0;     // boolean for if a player has won yet (0 or 1)
+    int roundCount = 0;  
+    int pos[9];         // this is our board (9 cells)
+    int index;          // index of the board (1-9)
+    int sign;           // either cross or naught (X or O)
+    int player;         // 1 or 2    
+    int flag;           // a variable to check win condition (if 3 of the same signs have been placed in a row)
+    int i, k, j;        // counters for loops
 
-    // initialise the board
+    // initialise each board cell with empty value
     for (i = 0; i < 9; i++) {
         pos[i] = ' ';
     }
 
-    while (count < 9 && winner != 1) {
+    while (roundCount < 9 && winner != 1) {
         flag = 0;
-        // print board
+        // print the tictactoe board
         printf("\n");
         printf("%c|%c|%c\n", pos[0], pos[1], pos[2]);
         printf("-+-+-\n");
@@ -36,7 +43,7 @@ int main() {
         printf("%c|%c|%c\n", pos[6], pos[7], pos[8]);
 
         // decide which player's turn it is
-        if (count % 2 == 0) {
+        if (roundCount % 2 == 0) {
             sign = 'X';
             player = 1;
         } else {
@@ -44,18 +51,18 @@ int main() {
             player = 2;
         }
 
-        printf("Move for player %d (1-9): ", player);
+        printf("Move for player %d: ", player);
         scanf("%d", &index);
+
+
         if (index < 1 || index > 9) {
             printf("Invalid index, choose from 1-9\n");
+        } else if (pos[index - 1] == 'X' || pos[index - 1] == 'O') {
+            printf("Position %d is occupied\n", index);
+        } else {
+            pos[index - 1] = sign;
+            roundCount++;
         }
-
-        if (pos[index - 1] == 'X' || pos[index - 1] == 'O') {
-            printf("Position is occupied\n");
-        }
-
-        pos[index - 1] = sign;
-        count++;
 
         for (i = 0; i < 9; i++) {
             if (i % 3 == 0) flag = 0;
